ee33fa95edfbd142cbbc72e6f325f23482221e2e
e <junkfoodsucks@gmail.com>
2013-04-02 12:59:16 +0200

    new test added

---
+++ test/ci/test_test.ci.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'selenium-webdriver'
+
+caps = Selenium::WebDriver::Remote::Capabilities.firefox
+caps[:browserName] = "firefox"
+caps[:version] = "16"
+caps[:platform] = :MAC
+
+
+driver = Selenium::WebDriver.for(
+  :remote,
+  :url => "http://192.168.5.62:4444/wd/hub",
+  :desired_capabilities => caps)
+  
+driver.navigate.to "http://www.nick.de"
+element = driver.find_element(:name, 'Nickelodeon Deutschland')
+#element.send_keys "Hello WebDriver!"
+#element.submit
+puts driver.title
+driver.quit
